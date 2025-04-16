98747baac4fce00d1a263b847b9a44d8fb6212d9
Cameron Eldridge <cameldridge@gmail.com>
2025-04-16 18:11:19 -0400

    modernize dependencies and get all building again

--- public_html/script/src/socket.js
+++ public_html/script/src/socket.js
@@ -1,10 +1,9 @@
 'use strict';
 import {default as io} from 'socket.io-client';
 import {set as setData} from './data.js';
-import {error, notification, success} from './notification.js';
+import {error, success} from './notification.js';
 
 export let socket = io();
 socket.on('error', error);
-socket.on('notification', notification);
 socket.on('success', success);
 socket.on('data:update', (d) => setData(JSON.parse(d)));
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
-'use strict';
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+"use strict";
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    mode: 'development',
-    entry: './public_html/script/src/index.js',
-    output: {
-        path: path.resolve('./public_html/'),
-        filename: 'script/tokaido.js'
-    },
-    module: {
-        rules: [
-            { test: /\.scss$/, use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'fast-sass-loader',
-            ]},
-            { test: /\.(otf|ttf)$/, use: 'url-loader' },
-            { test: /\.(svg|png|jpe?g)$/, use: 'url-loader?limit=5000&name=/image/[path][name].[ext]&context=public_html/image/src!image-webpack-loader' }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
+  mode: "development",
+  entry: "./public_html/script/src/index.js",
+  output: {
+    path: path.resolve("./public_html/"),
+    filename: "script/tokaido.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "fast-sass-loader"],
+      },
+      { test: /\.(otf|ttf)$/, type: "asset/resource" },
+      { test: /\.(svg|png|jpe?g)$/, type: "asset/resource" },
     ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
